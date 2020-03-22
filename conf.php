de31ee6e67602d8bd89661189a7ca8fa4b0f8b43
ALex.Liksagen <github-lxgn@ex.liksagen.com>
2020-03-22 16:16:18 +0300

    modify public node

--- conf.php
+++ conf.php
@@ -7,6 +7,8 @@ ini_set("error_reporting",1);
 
 #why 8840 see https://github.com/pro-blockchain-com/minter-node-nginx-upstream-checker
 $nodeUrl = 'http://127.0.0.1:8840';
+# OR PRO-BLOCKCHAIN.COM public node
+$nodeUrl = 'https://node-main.minter.su';
 
 $devider = 1000000000000000000;
 
